f84663105f860fe574126024be6a9762709299b5
Nazrin Shah Adrian Fernandez <nazrin2222@gmail.com>
2023-05-09 20:19:54 +0800

    Feat: brew install karabiner in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #install packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+#install karabiner elements for kbd
+brew install --cask karabiner-elements
