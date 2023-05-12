140ca181b845b16f20af15297a7a6204cddcc63a
Nazrin Shah Adrian Fernandez <nazrin2222@gmail.com>
2023-05-12 17:39:10 +0800

    Fix: Fixed binding for snippet selection

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,15 +1,14 @@
 local lsp = require('lsp-zero').preset({})
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+local luasnip = require('luasnip')
 
 lsp.preset("recommended")
-
 lsp.ensure_installed({
 	"tsserver",
 	"eslint",
 	"rust_analyzer",
 })
-
-local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
 
 -- Fixes vim not a global varaible in lua
 lsp.configure('lua_ls', {
@@ -32,6 +31,50 @@ lsp.configure('lua_ls', {
 		},
 	},
 })
+-- Fixes vim not a global varaible in lua
+
+-- symbols for neovim --
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+-- symbols for neovim --
+
+-- bind for error diagnostic
+vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>en', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ep', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fe', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
+-- bind for error diagnostic
+
+-- format on save --
+lsp.on_attach(function(client, bufnr)
+	lsp.default_keymaps({ buffer = bufnr })
+end)
+
+lsp.format_on_save({
+	format_opts = {
+		async = false,
+		timeout_ms = 10000,
+	},
+	servers = {
+		['lua_ls'] = { 'lua' },
+		['rust_analyzer'] = { 'rust' },
+	}
+})
+-- format on save --
+
+lsp.setup()
+
+
+-- for LuaSnip --
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+-- for LuaSnip --
 
 cmp.setup({
 	sources = {
@@ -47,35 +90,32 @@ cmp.setup({
 		-- Ctrl+Space to trigger completion menu
 		['<C-Space>'] = cmp.mapping.complete(),
 
+		-- Tab to go to next snippet jumpable if autocomplete is not open
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		-- S-Tab to go to previous snippet jumpable if autocomplete is not open
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
 		-- Navigate between snippet placeholder
 		['<C-n>'] = cmp_action.luasnip_jump_forward(),
 		['<C-p>'] = cmp_action.luasnip_jump_backward(),
 	}
 })
-
--- symbols for neovim
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
--- bind for error diagnostic
-vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>en', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ep', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fe', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
-
--- format on save
-lsp.on_attach(function(client, bufnr)
-	lsp.default_keymaps({ buffer = bufnr })
-end)
-
-lsp.format_on_save({
-	servers = {
-		['lua_ls'] = { 'lua' },
-		['rust_analyzer'] = { 'rust' },
-	}
-})
-
-lsp.setup()
